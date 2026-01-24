5d99db29c84d84233605d9062123cd47623acb6a
heshsaih <heshsaih@gmai.com>
2026-01-24 20:56:03 +0100

    feat: recognition of face and hand, sending both types of data

--- main.py
+++ main.py
@@ -6,13 +6,12 @@ from cvzone.FaceDetectionModule import FaceDetector
 import socket
 import urllib.request as urlreq
 import numpy as np
-
+import random
 
 def normalize(point):
     new_x = round(((point[0] - 0) / (1280)) * (7 - (-7)) + (-7), 5)
     new_y = round(((point[1] - 0) / (720)) * (4 - (-4)) + (-4), 5)
     return [new_x, new_y]
-
 
 cap = cv2.VideoCapture(0)
 
@@ -22,8 +21,6 @@ cap.set(4, 720)
 # hands
 detector = HandDetector(detectionCon=.8, maxHands=1)
 face_detector = FaceDetector()
-tolerance = 25
-previous_hands = []
 
 # face (stolen from "https://github.com/Danotsonof/facial-landmark-detection/tree/master")
 haarcascade_url = "https://raw.githubusercontent.com/opencv/opencv/master/data/haarcascades/haarcascade_frontalface_alt2.xml"
@@ -53,75 +50,73 @@ else:
     os.mkdir('data')
     urlreq.urlretrieve(LBFmodel_url, LBFmodel_file)
 
-landmark_detector = cv2.face.createFacemarkLBF()
+landmark_detector  = cv2.face.createFacemarkLBF()
 landmark_detector.loadModel(LBFmodel_file)
-
-previous_time = time.time()
-how_long_stable = 0
-sent = False
 
 socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 addr = ("127.0.0.1", 42069)
 
-while True:
-    current_time = time.time()
-    dt = current_time - previous_time
+wait_time = random.randint(3, 5)
+previous_time = time.time()
 
+while True:
     _, img = cap.read()
     hands, img = detector.findHands(img)
 
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
     faces = detector2.detectMultiScale(gray)
+    face = None
 
     for (x, y, w, d) in faces:
         _, landmarks = landmark_detector.fit(gray, np.array(faces))
 
-        for landmark in landmarks:
-            for x, y in landmark[0]:
-                cv2.circle(img, (int(x), int(y)), 1, (255, 0, 0), 2)
+        for x,y in landmarks[0][0]:
+            cv2.circle(img, (int(x), int(y)), 1, (255, 0, 0), 2)
 
-    # if hands and previous_hands:
-    #     current_landmarks = hands[0]["lmList"]
-    #     previous_landmarks = previous_hands[0]["lmList"]
-    #
-    #     if len(current_landmarks) == len(previous_landmarks):
-    #         is_stable = True
-    #
-    #         for i in range(len(current_landmarks)):
-    #             difference_x = abs(current_landmarks[i][0] - previous_landmarks[i][0])
-    #             difference_y = abs(current_landmarks[i][1] - previous_landmarks[i][1])
-    #
-    #             if difference_x > tolerance or difference_y > tolerance:
-    #                 is_stable = False
-    #                 sent = False
-    #                 break
-    #
-    #         if is_stable:
-    #             how_long_stable += dt
-    #         else:
-    #             how_long_stable = 0
-    #     else:
-    #         how_long_stable = 0
-    # else:
-    #     how_long_stable = 0
-    #
-    #
-    # if how_long_stable > 3 and not sent:
-    #     data = []
-    #     landmarks = previous_hands[0]["lmList"]
-    #
-    #     for (_, lm) in enumerate(landmarks):
-    #         new_point = normalize(lm)
-    #         data.append(new_point)
-    #
-    #     print("Sending data...")
-    #     socket.sendto(str.encode(str(data)), addr)
-    #     how_long_stable = 0
-    #     sent = True
+        face = landmarks[0][0]
 
-    previous_hands = hands
+    current_time = time.time()
+    if current_time - previous_time > wait_time:
+        hands_or_face = random.randint(0, 1)
+        found_value = False
+        to_send = None
 
-    previous_time = current_time
+        print(f"Chosen: {hands_or_face == 0 and 'hands' or 'face'}")
+
+        if hands_or_face == 0:
+            if hands:
+                to_send = hands[0]["lmList"]
+                found_value = True 
+            elif face is not None and face.any():
+                print("No hands data, fallback to face")
+                to_send = face
+                found_value = True 
+
+        if hands_or_face == 1:
+            if face is not None and face.any():
+                to_send = face
+                found_value = True 
+            elif hands:
+                print("No face data, fallback to hands")
+                to_send = hands[0]["lmList"]
+                found_value = True 
+
+        if found_value:
+            print("There is some data")
+            data = []
+
+            for (_, lm) in enumerate(to_send):
+                new_point = normalize(lm)
+                data.append(new_point)
+
+            print(f"Sending {hands_or_face == 0 and 'hands' or 'face'} data...")
+            socket.sendto(str.encode(f"{hands_or_face}" + str(data)), addr)
+        else:
+            print("No data")
+
+        wait_time = random.randint(3, 5)
+        previous_time = current_time
+
     cv2.imshow("Hand", img)
     cv2.waitKey(1)
